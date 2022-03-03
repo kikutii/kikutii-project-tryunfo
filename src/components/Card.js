f947c30c66b28a1c0952bd0e5201dd50ac505c67
kikutii <joaovictorkikuti@gmail.com>
2022-03-03 02:42:26 -0300

    troca de variável no alt da img

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,7 +20,7 @@ class Card extends React.Component {
         <h5 data-testid="attr1-card">{ cardAttr1 }</h5>
         <h5 data-testid="attr2-card">{ cardAttr2 }</h5>
         <h5 data-testid="attr3-card">{ cardAttr3 }</h5>
-        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <img data-testid="image-card" src={ cardImage } alt={ cardImage } />
         <h5 data-testid="rare-card">{ cardRare }</h5>
         {
           cardTrunfo ? <h5 data-testid="trunfo-card">Super Trunfo</h5> : ''

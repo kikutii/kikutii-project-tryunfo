7759a3faf05054b406f4193bf2f404636a103c72
kikutii <joaovictorkikuti@gmail.com>
2022-06-30 17:56:14 -0300

    adicionado componente returnando uma imagem svg

---
+++ src/components/StarSvg.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../style/ListCards.css';
+
+function StarSvg() {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 426.667 426.667"
+      xmlSpace="preserve"
+      className="cardStar"
+    >
+      <polygon
+        points="213.333,10.441 279.249,144.017
+         426.667,165.436 320,269.41 345.173,416.226
+         213.333,346.91 81.485,416.226 106.667,269.41 0,165.436
+         147.409,144.017"
+      />
+    </svg>
+  );
+}
+
+export default StarSvg;

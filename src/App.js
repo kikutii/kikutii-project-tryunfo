5774df4d3e14b6b09696d535dee733941e23662f
kikutii <joaovictorkikuti@gmail.com>
2022-06-30 19:00:18 -0300

    trocado link de imagem padrão do card

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
       cardAttr1: '90',
       cardAttr2: '90',
       cardAttr3: '30',
-      cardImage: 'https://c.tenor.com/bikpOygQKYEAAAAd/rem-re-zero-starting-life-in-another-world.gif',
+      cardImage: 'https://lever-client-logos.s3.us-west-2.amazonaws.com/1ecf7375-9cc9-4b3d-ba9a-c1cdcab267b5-1637340507739.png',
       cardRare: 'muito raro',
       cardTrunfo: true,
       minValue: 1,
